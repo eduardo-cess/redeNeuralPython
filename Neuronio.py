6aa1a066cfbedb9f254dd882df6e7da720b17c7a
Eduardo Santiago <eeduardosantiago@gmail.com>
2017-01-14 14:01:58 -0300

    modificações classe neuronio

--- Neuronio.py
+++ Neuronio.py
@@ -5,15 +5,14 @@ class Neuronio:
 
 	W = [] #lista de pesos
 	X = [] #lista de entradas
-	Yd = [] #saidas desejadas
-	Y = [] #saidas obtidas
+	Yd = 0 #saida desejada
+	Y = 0 #saida obtida
 
 
 	def __init__(self, entradas, saidasDesejadas):
 		super(Neuronio, self).__init__()
 		entradas.append(1)
 		self.X = np.array(entradas)
-		self.Yd = np.array(saidasDesejadas)
 		self.W = [np.random.random() for x in range(0,len(entradas))]
 
 
@@ -29,13 +28,16 @@ class Neuronio:
 	def funcaoTransferencia(self):
 		return 1/(1+np.exp(-(self.somatorioMatrix()))) #sigmoide
 
+	def saida(self):
+		self.Y = self.funcaoTransferencia()
+		return self.Y
 
 
-n = Neuronio([0.1,0.3,0.0010], [1,1,0])
 
-print("Entradas: ",n.X)
-print("Saídas desejadas: ",n.Yd)
-print(n.W)
-# print("Somatorio: ",n.somatorioNet())
-print("Somaorio: ", n.somatorioMatrix())
-print("Funcao de Transferencia: ",n.funcaoTransferencia())
+# n = Neuronio([0.1,0.3,0.0010], 1)
+
+# print("Entradas: ",n.X)
+# print("Saídas desejadas: ",n.Yd)
+# print(n.W)
+# print("Somatorio: ", n.somatorioMatrix())
+# print("Saída: ",n.saida())
